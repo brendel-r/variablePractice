26961dc1f80966ceae201698811ab91923c9eeb2
Rachel Brendel <brendelrachel@gmail.com>
2023-01-10 18:24:26 -0600

    reassigns some variables and adds console logs

--- variables.js
+++ variables.js
@@ -9,3 +9,12 @@ var yearMarried = 2015
 var isTuringStudent = true
 var isMarried = true
 var isHappy = true
+
+email = "newemail@gmail.com"
+address = "456 mad ln"
+favoriteNumber = 123
+
+console.log(email)
+console.log(favoriteNumber)
+console.log(isHappy)
+
